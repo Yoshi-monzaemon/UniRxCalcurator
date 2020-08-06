da3f531cd60c6f883c14c6b8a07eb6ed7fc0c0e3
Yoshi Monzaemon <49625730+Yoshi-monzaemon@users.noreply.github.com>
2020-08-06 23:38:03 +0900

    Retry

---
+++ CultivationSimulater/Assets/Scripts/FieldControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System.Collections.ObjectModel;
+
+public class FieldControl : MonoBehaviour
+{
+
+    //private Subject<int> 
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    private void Awake()
+    {
+        var eventTrigger = this.gameObject.AddComponent<ObservableEventTrigger>();
+
+        //PointerDown
+        eventTrigger.OnPointerDownAsObservable()
+            .Subscribe(pointerEventData =>
+            {
+                Debug.Log("kottidekenti");
+                
+            })
+            .AddTo(gameObject);
+    }
+
+
+}
---
+++ CultivationSimulater/Assets/Scripts/GrowPlant.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System.Collections.ObjectModel;
+using System;
+
+public class GrowPlant : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        
+    }
+
+    //発芽
+    public void sproutingPlant()
+    {
+
+    }
+
+    //開花
+    public void bloomingPlant()
+    {
+
+    } 
+
+    //結実
+    public void fruitingPlant()
+    {
+
+    }
+
+    public void GrowingPlant()
+    {
+        Observable.Timer(TimeSpan.FromSeconds(5))
+            .Subscribe(_ =>
+            {
+                Debug.Log("5secondsPassed");
+            })
+            .AddTo;
+    }
+}
